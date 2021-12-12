3aebeb1ea747b2633b9987ef0cd3f81d4dc2e6e9
Murolike <murolike@gmail.com>
2021-12-12 18:46:06 +0300

    Position Api
    - Position - entity
    - PositionRepository - CRUD
    - Add sql stack trace

---
+++ src/main/java/org/timesheet/humanresourcesdepartment/controllers/PositionController.java
@@ -0,0 +1,48 @@
+package org.timesheet.humanresourcesdepartment.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.timesheet.humanresourcesdepartment.models.core.Position;
+import org.timesheet.humanresourcesdepartment.models.repositories.PositionRepository;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+@RestController
+@RequestMapping("/position")
+public class PositionController {
+
+    @Autowired
+    private PositionRepository positionRepository;
+
+    @GetMapping("/index")
+    public Iterable<Position> index() {
+        return positionRepository.findAll();
+    }
+
+    @PostMapping("/add")
+    public Position add(@Valid @ModelAttribute Position position) {
+        return positionRepository.save(position);
+    }
+
+    @PostMapping("/update/{id}")
+    public Position update(@PathVariable Integer id, @Valid @ModelAttribute @NotNull Position position) {
+        Position storedPosition = findById(id);
+
+        storedPosition.setName(position.getName());
+        storedPosition.setIsActive(position.getIsActive());
+
+        positionRepository.save(storedPosition);
+
+        return storedPosition;
+    }
+
+    @GetMapping("/view/{id}")
+    public Position view(@PathVariable Integer id) {
+        return findById(id);
+    }
+
+    protected Position findById(Integer id) {
+        return positionRepository.findById(id).orElseThrow(() -> new RuntimeException("Position not found by id " + id));
+    }
+}
