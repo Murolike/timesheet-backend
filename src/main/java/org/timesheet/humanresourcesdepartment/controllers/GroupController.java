0a1a8122d810cf76ec920da39a25ab089df26897
Murolike <murolike@gmail.com>
2022-02-14 11:40:48 +0300

    GroupController
    - Clear code

--- src/main/java/org/timesheet/humanresourcesdepartment/controllers/GroupController.java
+++ src/main/java/org/timesheet/humanresourcesdepartment/controllers/GroupController.java
@@ -1,23 +1,11 @@
 package org.timesheet.humanresourcesdepartment.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
 import org.timesheet.humanresourcesdepartment.models.core.Group;
 import org.timesheet.humanresourcesdepartment.models.repositories.GroupRepository;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/group")
